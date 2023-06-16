00b8be87c2989e0a59a95dd37c5f23c4b3b68136
thlim <thlim@crscube.co.kr>
2023-06-16 12:14:27 +0900

    AppBar와 BottomNavigationBar 달기

--- lib/common/layout/default_layout.dart
+++ lib/common/layout/default_layout.dart
@@ -1,20 +1,43 @@
 import 'package:flutter/material.dart';
 
 class DefaultLayout extends StatelessWidget {
-  final Widget child;
   final Color backgroundColor;
+  final Widget child;
+  final String? title;
+  final Widget? bottomNavigationBar;
 
   const DefaultLayout({
     super.key,
-    required this.child,
     this.backgroundColor = Colors.white,
+    required this.child,
+    this.title,
+    this.bottomNavigationBar,
   });
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: backgroundColor,
+      appBar: renderAppBar(title),
       body: child,
+      bottomNavigationBar: bottomNavigationBar,
+    );
+  }
+}
+
+AppBar? renderAppBar(String? title) {
+  if (title == null) {
+    return null;
+  } else {
+    return AppBar(
+      backgroundColor: Colors.white,
+      elevation: 0,
+      title: Text(
+        title,
+        style: TextStyle(
+          color: Colors.black,
+        ),
+      ),
     );
   }
 }
--- lib/common/view/root_tab.dart
+++ lib/common/view/root_tab.dart
@@ -1,14 +1,54 @@
+import 'package:actual/common/const/colors.dart';
 import 'package:actual/common/const/data.dart';
 import 'package:actual/common/layout/default_layout.dart';
 import 'package:actual/user/view/login_screen.dart';
 import 'package:flutter/material.dart';
 
-class RootTab extends StatelessWidget {
+class RootTab extends StatefulWidget {
   const RootTab({super.key});
+
+  @override
+  State<RootTab> createState() => _RootTabState();
+}
+
+class _RootTabState extends State<RootTab> {
+  int index = 0;
 
   @override
   Widget build(BuildContext context) {
     return DefaultLayout(
+      title: '코팩 딜리버리',
+      bottomNavigationBar: BottomNavigationBar(
+        selectedItemColor: primaryColor,
+        unselectedItemColor: bodyTextColor,
+        selectedFontSize: 10,
+        unselectedFontSize: 10,
+        type: BottomNavigationBarType.shifting,
+        onTap: (int index) {
+          setState(() {
+            this.index = index;
+          });
+        },
+        currentIndex: index,
+        items: [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: '홈',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.search),
+            label: '검색',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.shopping_cart),
+            label: '장바구니',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.person),
+            label: '마이페이지',
+          ),
+        ],
+      ),
       child: GestureDetector(
         onTap: () async {
           await storage.deleteAll();
